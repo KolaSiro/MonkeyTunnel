de250c239034846f6c4e17763da252980130ed42
Kurt Müllner <kurt.muellner@bluewin.ch>
2024-02-16 15:50:57 +0100

    Initial version

--- MonkeyApp1/Program.cs
+++ MonkeyApp1/Program.cs
@@ -48,7 +48,7 @@ app.MapMonkeyEndpoints();
 app.MapControllers();
 
 app.Run();
-
+                    
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
